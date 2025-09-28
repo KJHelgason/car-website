5ee17577eb5e94b423c40d6138b819548e665063
BuildTools <unconfigured@null.spigotmc.org>
2025-09-28 01:59:33 +0000

    finally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+//import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import "./globals.css";
 import Script from "next/script";
 
